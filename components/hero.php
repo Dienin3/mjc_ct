b494fe92dc64abcad6c01f9d611140202a848ae4
Dienin3 <Dienin66@gmail.com>
2022-07-05 14:41:23 +1200

    Began coding main page hero
    not much progress due to certain coding issues will make more progress

---
+++ components/hero.php
@@ -0,0 +1,7 @@
+<div class="hero-image">
+  <div class="hero-text">
+    <h1>MJC: Teaching & Consulting</h1>
+    <p>Sustainable blah blah</p>
+    <button>Learn More</button>
+  </div>
+</div>
--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
 
-<html>
+<html lang="en">
     <head>
+        <meta charset="UTF-8">
         <link rel="stylesheet" href="css/style.css" />
         <script type="text/javascript" src="/js/javascript.js"></script>
     </head>
     
     <body>
         <?php include('components/header.php');?>
-
+            <?php include('components/hero.php')?>
         <?php include('components/footer.php');?>
     </body>
 </html>
