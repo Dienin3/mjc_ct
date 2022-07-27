0b8399113199a216bc65f87500302b60ee71eb4d
Dienin3 <Dienin66@gmail.com>
2022-07-27 12:11:31 +1200

    learned how to link database to page
    fitted code and sections from main page into database.

--- index.php
+++ index.php
@@ -7,19 +7,39 @@
         <script type="text/javascript" src="/js/javascript.js"></script>
         <?php include('components/header.php');?>
     </head>
+<?php
+    include('setup.php');
+    $sql = "Select title1, para1, img1 from pages where id=1";
+    $result = $conn->query($sql);
 
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    
+        $title1 = $row["title1"];
+        $para1 =$row["para1"];
+        $img1 = $row["img1"];
+      
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+    ?>
+    
     <body>
         <?php include('components/hero.php')?>
         <div class="home-section">
                 <div class="home-section-text">
-                    <h1><u>What do I offer?</u></h1>
-                    <h2><i>My Experience</i></h2>
-                    <span>My name is Marcelo J. Carena, <br>I have worked both as an academic leader, and as a teacher, in various Universities and Organizations. As an academic leader, I've always been people-oriented and achievement focused working with the education of students and mentoring faculty colleagues for teaching practice and research direction. <br>My commitment to teaching and learning has been continuous for over 20 years, and I have over 30 years of experience in the Ag business. I have continuously applied innovative approaches to teaching methods and practice in support of teaching excellence. I established unique content and teaching techniques in the area of plant sciences by considering the diversity of the student population and my active participation in teaching circles and national teaching associations. <br>I enjoyed tertiary teaching for a long time and still, after so many years, it lifts my spirit when I lecture, most recently virtually for a tertiary course at the University of Buenos Aires during COVID-19 last year. I have been a strong learner with the customer always in mind when marketing education and I would like to share my experiences to those around me, and my students, by challenging them with unique ideas. </span>
+                    <h1><u><?php print $title1 ?></u></h1>
+                        <?php print $para1 ?>
                 </div>
                 <div>
-                    <img class="home-img-1" src="images/Hp-2.jpg">
+                    <img class="home-img-1" src="images/<?php print $img1 ?>">
                 </div>
             </div>
+        
+        
     </body>
     
     <footer>
