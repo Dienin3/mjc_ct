4df2319df43d60ee596d7360443c27dfb0ac1344
Dienin3 <Dienin66@gmail.com>
2022-08-03 17:30:50 +1200

    fixed navbar disappearing issue
    fixed navbar disappearing issue.
    fixed responsivity of main page/about me page.

--- js/javascript.js
+++ js/javascript.js
@@ -1,19 +1,20 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-const navLink = document.querySelectorAll(".nav-link");
-
-
-hamburger.addEventListener("click", mobileMenu);
-navLink.forEach(n => n.addEventListener("click", closeMenu));
+var hamburger = document.querySelector(".hamburger");
+var navMenu = document.querySelector(".nav-menu");
+var navLink = document.querySelectorAll(".nav-link");
 
 //adds active tag to open menu
 function mobileMenu() {
+    "use strict";
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 }
 
 //removes active tag to close menu
 function closeMenu() {
+    "use strict";
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+hamburger.addEventListener("click", mobileMenu);
+
