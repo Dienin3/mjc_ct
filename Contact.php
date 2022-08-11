dde1dd099d382158e0c978c2e24c47ffdab7008b
Dienin3 <Dienin66@gmail.com>
2022-08-11 13:39:44 +1200

    Began contact page including 'contact me' form with php

--- Contact.php
+++ Contact.php
@@ -5,9 +5,13 @@
         <script type="text/javascript" src="/js/javascript.js"></script>
         <?php include('components/header.php');?>
     </head>
+    <?php 
+// Form submission script 
+include_once 'submit.php'; 
+?>
 <?php
     include('setup.php');
-    $sql = "Select  from pages where pagenum=5";
+    $sql = "Select title1 from pages where pagenum=3";
     $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
@@ -26,7 +30,33 @@ $conn->close();
    
     <body>
         <div class="section contactHero">
-            
+            <?php print $title1 ?>
+            <!-- Status message -->
+<?php if(!empty($statusMsg)){ ?>
+    <div class="status-msg <?php echo $status; ?>"><?php echo $statusMsg; ?></div>
+<?php } ?>
+
+<!-- Form fields -->
+<form action="" method="post">
+    <div class="form-input">
+        <label for="name">Name</label>
+        <input type="text" name="name" placeholder="Enter your name" value="<?php echo !empty($postData['name'])?$postData['name']:''; ?>" required="">
+    </div>
+    <div class="form-input">
+        <label for="email">Email</label>
+        <input type="email" name="email" placeholder="Enter your email" value="<?php echo !empty($postData['email'])?$postData['email']:''; ?>" required="">
+    </div>
+    <div class="form-input">
+        <label for="subject">Subject</label>
+        <input type="text" name="subject" placeholder="Enter subject" value="<?php echo !empty($postData['subject'])?$postData['subject']:''; ?>" required="">
+    </div>
+    <div class="form-input">
+        <label for="message">Message</label>
+        <textarea name="message" placeholder="Type your message here" required=""><?php echo !empty($postData['message'])?$postData['message']:''; ?></textarea>
+    </div>
+    
+    <input type="submit" name="submit" class="btn" value="Submit">
+</form>
         </div>
     </body>
     
