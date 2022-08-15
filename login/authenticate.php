334c4849cd67e4bd90966918b809f1c9f885b77a
Dienin3 <Dienin66@gmail.com>
2022-08-15 14:30:11 +1200

    Did fixes to authenticate.php file and linked profile page to login

--- login/authenticate.php
+++ login/authenticate.php
@@ -1,12 +1,9 @@
 <?php
 session_start();
 // Change this to your connection info.
-$DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'root';
-$DATABASE_PASS = '';
-$DATABASE_NAME = 'ctg';
+include('../setup.php');
 // Try and connect using the info above.
-$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+$con = mysqli_connect($servername, $username, $password, $db);
 if ( mysqli_connect_errno() ) {
 	// If there is an error with the connection, stop the script and display the error.
 	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
@@ -36,7 +33,7 @@ if ($stmt->num_rows > 0) {
 		$_SESSION['loggedin'] = TRUE;
 		$_SESSION['name'] = $_POST['username'];
 		$_SESSION['id'] = $id;
-		header('Location: home.php');
+		header('Location: ../index.php');
 	} else {
 		// Incorrect password
 		echo 'Incorrect username and/or password!';
--- profile.php
+++ profile.php
@@ -1,13 +1,19 @@
 <html lang="en">
+    <?php 
+    session_start();
+            if (!isset($_SESSION['loggedin'])){
+                header('Location: login/login.php');
+            } else {
+            }
+?>
     <head>
         <meta charset="UTF-8">
         <link rel="stylesheet" href="css/style.css" />
         <script type="text/javascript" src="/js/javascript.js"></script>
-        <?php include('components/header.php');?>
     </head>
     
     <body>
-        
+        <?php include('components/header.php');?>
     </body>
     
     <footer>
