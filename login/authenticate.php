fbac0faf4ce3288d84aff7bf111a850046697045
Dienin3 <Dienin66@gmail.com>
2022-08-17 11:15:41 +1200

    Revert "Did fixes to authenticate.php file and linked profile page to login"
    This reverts commit 334c4849cd67e4bd90966918b809f1c9f885b77a.

--- login/authenticate.php
+++ login/authenticate.php
@@ -1,9 +1,12 @@
 <?php
 session_start();
 // Change this to your connection info.
-include('../setup.php');
+$DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'root';
+$DATABASE_PASS = '';
+$DATABASE_NAME = 'ctg';
 // Try and connect using the info above.
-$con = mysqli_connect($servername, $username, $password, $db);
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if ( mysqli_connect_errno() ) {
 	// If there is an error with the connection, stop the script and display the error.
 	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
@@ -33,7 +36,7 @@ if ($stmt->num_rows > 0) {
 		$_SESSION['loggedin'] = TRUE;
 		$_SESSION['name'] = $_POST['username'];
 		$_SESSION['id'] = $id;
-		header('Location: ../index.php');
+		header('Location: home.php');
 	} else {
 		// Incorrect password
 		echo 'Incorrect username and/or password!';
--- profile.php
+++ profile.php
@@ -1,19 +1,13 @@
 <html lang="en">
-    <?php 
-    session_start();
-            if (!isset($_SESSION['loggedin'])){
-                header('Location: login/login.php');
-            } else {
-            }
-?>
     <head>
         <meta charset="UTF-8">
         <link rel="stylesheet" href="css/style.css" />
         <script type="text/javascript" src="/js/javascript.js"></script>
+        <?php include('components/header.php');?>
     </head>
     
     <body>
-        <?php include('components/header.php');?>
+        
     </body>
     
     <footer>
